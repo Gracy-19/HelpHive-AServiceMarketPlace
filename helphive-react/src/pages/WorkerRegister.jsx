c977930c8db47cdd08c59443c1d28a4923e4f9b9
Gracy Patel <gracypatel1921@gmail.com>
2025-11-11 11:42:10 +0530

    Update WorkerRegister.jsx

--- helphive-react/src/pages/WorkerRegister.jsx
+++ helphive-react/src/pages/WorkerRegister.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
 import { Calendar, MapPin, Phone, User, FileText } from "lucide-react";
 
-const API = "http://localhost:4000";
+const API = import.meta.env.VITE_API_URL;
 
 const WorkerDashboard = () => {
   const { user } = useUser();
